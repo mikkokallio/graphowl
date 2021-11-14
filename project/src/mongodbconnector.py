0cfc4631643b5f7b661c1b8673877a75d702600e
Mikko Kallio <mikkokallio@microsoft.com>
2021-11-14 10:51:23 +0200

    Add encapsulation and improve code

--- project/src/mongodbconnector.py
+++ project/src/mongodbconnector.py
@@ -4,20 +4,19 @@ import pymongo
 class MongoDbConnector:
     """Establishes connection to a particular database of a particular MongoDb instance and loads data into appropriate format"""
 
-    def __init__(self, uri, cert, db) -> None:
-        self.client = pymongo.MongoClient(uri, tls=True, tlsCertificateKeyFile=cert)
-        self.db = self.client[db]
+    def __init__(self, uri: str, cert: str, db: str) -> None:
+        self._client = pymongo.MongoClient(uri, tls=True, tlsCertificateKeyFile=cert)
+        self._db = self._client[db]
 
-    def transform(self, data):
+    def _transform(self, data) -> dict[str, tuple[list, list]]:
         plots = {row['name']:([],[]) for row in data}
         for row in data:
             plots[row['name']][0].append(row['time'])
             plots[row['name']][1].append(row['value'])
         return plots
         
-    def get_data(self, colname: str, time: str, value: str, name: str) -> str:
-        coll = self.db[colname]
+    def get_data(self, collname: str, time: str, value: str, name: str) -> str:
+        coll = self._db[collname]
         result = coll.find({},{ '_id': 0, time: 1, value: 1, name: 1 })
-        rawdata = [{time: row[time], 'value': row[value], name: row[name]} for row in result]
-        pre_processed = sorted(rawdata, key=lambda row: row['name'])
-        return self.transform(pre_processed)
+        data = [{time: row[time], 'value': row[value], name: row[name]} for row in result]
+        return self._transform(data)
