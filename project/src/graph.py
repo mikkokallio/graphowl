228d213e322138fa600debecf696d129f9649a27
Mikko Kallio <mikkokallio@microsoft.com>
2021-11-14 17:36:08 +0200

    Add docstring

--- project/src/graph.py
+++ project/src/graph.py
@@ -1,7 +1,17 @@
 class Graph:
-    """One graph data handling object in the dashboard"""
+    """A data handling object representing a graph widget in the dashboard"""
 
-    def __init__(self, title, connector, collection, time, value, name):
+    def __init__(self, title: str, connector: str, collection: str, time: str, value: str, name: str):
+        """Creates a graph data object with all metadata necessary for pulling data and displaying it in a graph widget.
+
+        Args:
+            title (str): Name of the graph widget.
+            connector (str): Name of connector for pulling data.
+            collection (str): Name of collection in a database.
+            time (str): Name of the time column in the collection.
+            value (str): Name of the value column in the collection.
+            name (str): Name of the name column in the collection.
+        """
         self.title = title
         self.connector = connector
         self.collection = collection
@@ -10,4 +20,9 @@ class Graph:
         self.name = name
     
     def load(self):
+        """Uses the connector to pull data from the data source.
+
+        Returns:
+            [dict]: Data to show in the corresponding UI widget.
+        """
         return self.connector.get_data(self.collection, time=self.time, value=self.value, name=self.name)
