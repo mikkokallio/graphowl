a0dab1d722b29683ba5b5dcdfadf872393d5c9b6
Mikko Kallio <mikkokallio@microsoft.com>
2021-11-15 19:39:04 +0200

    Enhance graph visuals

--- project/src/tester_app.py
+++ project/src/tester_app.py
@@ -19,10 +19,10 @@ class UI:
         COLOR_GRID='#2A3459'
         COLOR_LITE='#3A4469'
         COLOR_BRITE='#7A84A9'
-        layout = {'x': 2, 'y': 2}
+        layout = {'x': dashboard.layout['x'], 'y': dashboard.layout['y']}
 
-        fig = Figure(figsize=(10, 7), dpi=80, facecolor=COLOR_DARKER, edgecolor=COLOR_GRID, linewidth=1.0)
-        gs = fig.add_gridspec(layout['y'], layout['x'], hspace=0.4)
+        fig = Figure(figsize=(12, 8), dpi=100, facecolor=COLOR_DARKER, edgecolor=COLOR_GRID, linewidth=1.0)
+        gs = fig.add_gridspec(layout['y'], layout['x'], hspace=0.5)
         ax = [[None for _ in range(layout['x'])] for _ in range(layout['y'])]
 
         data = dashboard.load_all()
@@ -41,11 +41,19 @@ class UI:
                 if graph_n >= len(data): continue
                 ax[y][x].set_title(data[graph_n][0], fontdict={'color':'white','size':10})
 
+                mini, maxi = 100000, 0
                 for plot in data[graph_n][1].values():
-                    ax[y][x].plot(plot[0], plot[1], marker='o', path_effects=[patheffects.SimpleLineShadow(),
-                       patheffects.Normal()])
-                    ax[y][x].fill_between(x=plot[0], y1=plot[1], y2=min(plot[1]) * 0, alpha=0.045)
+                    if min(plot[1]) < mini: mini = min(plot[1])
+                    if max(plot[1]) > maxi: maxi = max(plot[1])
+
+                for graph in data[graph_n][1]:
+                    plot = data[graph_n][1][graph]
+                    ax[y][x].plot(plot[0], plot[1], marker='o', markersize=2.5, path_effects=[patheffects.SimpleLineShadow(),
+                       patheffects.Normal()], label=graph)
+                    ax[y][x].fill_between(x=plot[0], y1=plot[1], y2=mini, alpha=0.025)
                 graph_n += 1
+                ax[y][x].legend(loc='lower center', bbox_to_anchor=(0.5, -0.35), labelcolor='white', facecolor='black', framealpha=0.3, edgecolor='none', ncol=3)
+
 
         return FigureCanvasTkAgg(fig, master=self._root)
 
@@ -71,15 +79,14 @@ class UI:
         left_pane5_label.grid(row=6, column=1, padx=5, pady=5)
         canvas.get_tk_widget().grid(row=1, rowspan=6, column=3, padx=5, pady=5)
 
-        self._root.grid_columnconfigure(1, weight=1, minsize=50)
+        self._root.grid_columnconfigure(1, weight=1, minsize=100)
 
 loader = ConfigHandler('config/dashboard.yaml')
 dashboard = loader.load()
 
 db_data = dashboard.load_all()
-for x in db_data:
-    print(x)
-
+#for x in db_data:
+#    print(x)
 
 window = Tk()
 window.title("GraphOwl")
