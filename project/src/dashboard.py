93e22f67dfd460a9d2e19b890f6267732e71a0fd
Mikko Kallio <mikkokallio@microsoft.com>
2021-11-16 17:03:41 +0200

    Add docstring

--- project/src/dashboard.py
+++ project/src/dashboard.py
@@ -5,7 +5,18 @@ from graph import Graph
 class Dashboard:
     """Contains all graphs and other configuration and data"""
 
-    def __init__(self, title, layout, timespan, interval, sources, graphs):
+    def __init__(self, title: str, layout: dict, timespan: str, interval: str, sources: list, graphs: list):
+        """Creates a dashboard data object, which is the interface between the UI and data collection.
+
+        Args:
+            title (str): Title for the entire dashboard.
+            layout (dict): Indicates how many graphs are stacked horizontally (x) and vertically (y).
+            timespan (str): How many hours/days worth of data is fecthed and displayed.
+            interval (str): How often the graphs update.
+            sources (list): Databases and other sources to fetch data from.
+            graphs (list): Graphs to display in the layout.
+        """
+
         self.title = title
         self.layout = layout
         self.timespan = timespan
@@ -20,4 +31,9 @@ class Dashboard:
             self.graphs.append(Graph(graph['title'], self.sources[graph['connector']], graph['collection'], graph['fields']))
 
     def load_all(self):
+        """Requests all graphs in the dashboard to pull data from their connectors.
+
+        Returns:
+            list: List where each item contains data for one graph.
+        """
         return [{'title': graph.title, 'plots': graph.load()} for graph in self.graphs]
