7a80c13a45020b09388255acaec50b9139c16408
Mikko Kallio <mikkokallio@microsoft.com>
2021-11-16 07:40:37 +0200

    Simplify and improve structure

--- project/src/tester_app.py
+++ project/src/tester_app.py
@@ -3,7 +3,6 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 from matplotlib import patheffects
-from mongodbconnector import MongoDbConnector
 from confighandler import ConfigHandler
 from graphwidget import GraphWidget
 from constants import *
@@ -15,50 +14,38 @@ class UI:
     def draw_graph_widget(self):
         pass
 
-    def draw_coordinates(self, ax):
+    def draw_coordinates(self, ax, data):
+        """Draw background for the graph widget"""
         ax.tick_params(color=COLOR_LITE, labelcolor=COLOR_BRITE)
         ax.grid(color=COLOR_GRID)
         [x.set_color(COLOR_LITE) for x in [ax.spines.top, ax.spines.bottom, ax.spines.left, ax.spines.right]]
+
+        if data is not None:
+            ax.set_title(data['title'], fontdict={'color':'white','size':10})
+            smallest = min([min(plot[1]) for plot in data['plots'].values()])
+
+            for title, plot in data['plots'].items():
+                ax.plot(plot[0], plot[1], marker='o', markersize=2.5, path_effects=[patheffects.SimpleLineShadow(),
+                    patheffects.Normal()], label=title)
+                ax.fill_between(x=plot[0], y1=plot[1], y2=smallest, alpha=0.025)
+            ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.35), labelcolor='white', facecolor='black', framealpha=0.3, edgecolor='none', ncol=3)
         return ax
 
-    def draw_graphs(self, dashboard):
-        layout = {'x': dashboard.layout['x'], 'y': dashboard.layout['y']}
-
+    def draw_graphs(self, rows, cols, graphdata):
+        """Uses a generator to yield one graph at a time to put into the layout"""
         fig = Figure(figsize=(12, 8), dpi=100, facecolor=COLOR_DARKER, edgecolor=COLOR_GRID, linewidth=1.0)
-        gs = fig.add_gridspec(layout['y'], layout['x'], hspace=0.5)
-        axs = [[None for _ in range(layout['x'])] for _ in range(layout['y'])]
+        gs = fig.add_gridspec(cols, rows, hspace=0.5)
 
-        data = dashboard.load_all()
+        graph_gen = (graph for graph in graphdata)
 
-        graph_n = 0
-
-        for y in range(layout['y']):
-            for x in range(layout['x']):
-                #ax[y][x] = self.draw_graph_widget()
-                ax = fig.add_subplot(gs[y, x], polar=False, frameon=True, facecolor=COLOR_DARK)
-                if graph_n >= len(data): continue
-                ax.set_title(data[graph_n][0], fontdict={'color':'white','size':10})
-                ax = self.draw_coordinates(ax)
-
-                mini, maxi = 100000, 0
-                for plot in data[graph_n][1].values():
-                    if min(plot[1]) < mini: mini = min(plot[1])
-                    if max(plot[1]) > maxi: maxi = max(plot[1])
-
-                for graph in data[graph_n][1]:
-                    plot = data[graph_n][1][graph]
-                    ax.plot(plot[0], plot[1], marker='o', markersize=2.5, path_effects=[patheffects.SimpleLineShadow(),
-                       patheffects.Normal()], label=graph)
-                    ax.fill_between(x=plot[0], y1=plot[1], y2=mini, alpha=0.025)
-                ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.35), labelcolor='white', facecolor='black', framealpha=0.3, edgecolor='none', ncol=3)
-                axs[y][x] = ax
-                graph_n += 1
-
-
+        for y in range(rows):
+            for x in range(cols):
+                ax = fig.add_subplot(gs[y, x], frameon=True, facecolor=COLOR_DARK)
+                self.draw_coordinates(ax, next(graph_gen, None))
         return FigureCanvasTkAgg(fig, master=self._root)
 
 
-    def start(self, dashboard):
+    def start(self):
         logo_label = ttk.Label(master=self._root, text="G")
         left_pane1_label = ttk.Label(master=self._root, text="1")
         left_pane2_label = ttk.Label(master=self._root, text="2")
@@ -66,7 +53,9 @@ class UI:
         left_pane4_label = ttk.Label(master=self._root, text="4")
         left_pane5_label = ttk.Label(master=self._root, text="5")
 
-        canvas = self.draw_graphs(dashboard)
+        loader = ConfigHandler('config/dashboard.yaml')
+        dashboard = loader.load()
+        canvas = self.draw_graphs(dashboard.layout['y'], dashboard.layout['x'], dashboard.load_all())
         canvas.draw()
 
         # Layout
@@ -81,17 +70,10 @@ class UI:
 
         self._root.grid_columnconfigure(1, weight=1, minsize=100)
 
-loader = ConfigHandler('config/dashboard.yaml')
-dashboard = loader.load()
-
-db_data = dashboard.load_all()
-#for x in db_data:
-#    print(x)
-
 window = Tk()
 window.title("GraphOwl")
 
 ui = UI(window)
-ui.start(dashboard)
+ui.start()
 
 window.mainloop()
