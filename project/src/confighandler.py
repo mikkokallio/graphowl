4b7236308a67bcb32429393958d55e125ea4564a
Mikko Kallio <mikkokallio@microsoft.com>
2021-11-19 19:01:34 +0200

    Add docstring

--- project/src/confighandler.py
+++ project/src/confighandler.py
@@ -5,10 +5,22 @@ from dashboard import Dashboard
 class ConfigHandler:
     """Loads yaml files and creates dashboard objects"""
 
-    def __init__(self, filepath) -> None:
+    def __init__(self, filepath: str) -> None:
+        """Creates a configuration handler object related to a particular dashboard configuration.
+
+        Args:
+            filepath (str): Path to the configuration file
+        """
+
         self.filepath = filepath
     
     def load(self) -> Dashboard:
+        """Loads configuration from a yaml file and creates a dashboard from it.
+
+        Returns:
+            Dashboard: The resulting dashboard data object.
+        """
+        
         file = open(self.filepath, 'r')
         config = yaml.safe_load(file)
 
